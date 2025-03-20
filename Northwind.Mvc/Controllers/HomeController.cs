fd6e1b1cd52ab4110d4bfe4b7d8554d378f2f932
Emelie <emelieaxi@hotmail.com>
2025-03-20 10:50:41 +0100

    ProductDetail Action Method

--- Northwind.Mvc/Controllers/HomeController.cs
+++ Northwind.Mvc/Controllers/HomeController.cs
@@ -28,6 +28,21 @@ namespace Northwind.Mvc.Controllers
 
             return View(model);
         }
+        public IActionResult ProductDetail(int? id)
+        {
+            if (!id.HasValue)
+            {
+                return BadRequest("Product ID is missing");
+            }
+            
+            Product? model = db.Products.SingleOrDefault(p => p.ProductId == id);
+            if (model == null)
+            {
+                return NotFound($"Product with ID of {id} not found.");
+            }
+
+            return View(model);
+        }
 
         public IActionResult Privacy()
         {
