6d932a5d1fe196b575bea7212b4971bb9eec8402
Emelie <emelieaxi@hotmail.com>
2025-03-20 12:02:15 +0100

    ModelBindning

---
+++ Northwind.Mvc/Models/HomeModelBindningViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Northwind.Mvc.Models
+{
+    public record HomeModelBindningViewModel(
+        Thing thing,
+        bool HasErrors,
+        IEnumerable<string> ValidationsErrors
+        );
+}
